8cfbce55731ef0a64cf1a95dc0adb17914bdfe29
Bazger <ivan.zolotaryov@gmail.com>
2020-03-28 19:14:29 +0300

    Small comment change

--- src/main/java/com/finityx/Main.java
+++ src/main/java/com/finityx/Main.java
@@ -6,7 +6,7 @@ import com.finityx.common.Application;
 public class Main {
 
     //For running this app add args to java command line
-    //Example: app_name=FinityXApp config_path=$PATH_TO_FILE/app.properties mode=batch
+    //Example: app_name=FinityXApp config_path=.\env\app.properties mode=batch
     public static void main(String[] args) throws Exception {
         new Application<>().run(BinaryTreeStreamingContext::new, args);
     }
